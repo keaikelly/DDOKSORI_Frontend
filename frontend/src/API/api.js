8df565456cf08ab9f171fddbeb768b0a6cd9c848
MJ <nampink2300@gmail.com>
2025-01-12 06:14:15 +0900

    [fix] 오류 수정되라

--- frontend/src/API/api.js
+++ frontend/src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://2c01-210-94-220-228.ngrok-free.app",
 
   headers: {
     "Content-Type": "application/json"

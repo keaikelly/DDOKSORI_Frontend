195ad56529eaf51e2f139e5d5bef3655ad44bdd4
MJ <nampink2300@gmail.com>
2025-01-11 22:02:27 +0900

    [feat] 목표 등록 팝업 생성

--- frontend/src/pages/MyPage/MyPage/MyPage.js
+++ frontend/src/pages/MyPage/MyPage/MyPage.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MyPage.module.css";
 import Button from "../../../components/common/button/button";
 import SmallButton from "../../../components/common/smallButton/smallButton";
 import ListComponent from "../../../components/MyPage/listComponent/listComponent";
 import PlusListComponent from "../../../components/MyPage/listComponent/plusListComponent.js";
 import BackButton from "../../../components/common/backbutton/backbutton.js";
+import InputPopup from "../../../components/common/InputPopup/InputPopup.js";
 
 const MyPage = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handlePopupOpen = () => {
+    setShowPopup(true); // 팝업 열기
+  };
+
+  const handlePopupClose = () => {
+    setShowPopup(false); // 팝업 닫기
+  };
+
   return (
     <div className={styles.container}>
+      {showPopup && (
+        <InputPopup 
+          title={"나의 목표 입력하기"} 
+          text={"목표 입력하기"} 
+          buttonText={"목표 저장하기"} 
+          close={handlePopupClose} 
+        />
+      )}
       <div className={styles.titleContainer}>
-        <BackButton/>
+        <BackButton />
         <div className={styles.title}>내 버킷노트</div>
       </div>
       <div className={styles.context}>
@@ -20,13 +39,10 @@ const MyPage = () => {
 
       <div className={styles.listContainer}>
         <div className={styles.listComponents}>
-
-        <PlusListComponent/>
-            <ListComponent text="삐약톤 참가하기" link="/detail/1"/>
-            <ListComponent text="삐약톤 참가하기" link="/detail/1"/>
-
+          <PlusListComponent onClick={handlePopupOpen} />
+          <ListComponent text="삐약톤 참가하기" link="/detail/1" />
+          <ListComponent text="삐약톤 참가하기" link="/detail/1" />
         </div>
-        <div></div>
         <SmallButton text={"이전 노트 보기"} link={"/myprevious"} />
       </div>
       <div className={styles.buttonContainer}>

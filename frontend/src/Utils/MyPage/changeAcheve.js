cd471a41b0340b24f96a2305d87702aa988ccb13
MJ <nampink2300@gmail.com>
2025-01-12 09:38:04 +0900

    [fix] 달성 여부 반영

--- frontend/src/Utils/MyPage/changeAcheve.js
+++ frontend/src/Utils/MyPage/changeAcheve.js
@@ -4,17 +4,17 @@ export const changeAcheve = async (token, isAchived, bucketListId, setLoading) =
   // 이미 로딩 중인 경우 함수 실행을 중지
   if (setLoading) setLoading(true);
 
-  const endpoint = `/api/bucketList/${bucketListId}/achieve`;
+  const endpoint = `/api/bucketList/${bucketListId}/achieve?isAchieved=${isAchived}`;
   try {
     // API 요청
     console.log("되라");
     console.log(isAchived);
     await API.put(endpoint, {
-        isAchived: isAchived, // goalText에 text 값 전달
+       
     }, {
       headers: {
         Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json', // 헤더 설정 시 Content-Type은 대소문자 구분을 유의
+        'Content-Type': 'accept: */*', // 헤더 설정 시 Content-Type은 대소문자 구분을 유의
       },
     });
 

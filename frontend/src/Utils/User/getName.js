15135ab56187329dc9e81828366e8611efca997d
okiidokim <scw09268@dgu.ac.kr>
2025-01-12 09:46:25 +0900

    [API] 검색 완료, 닉네임 관련 진행 중
    닉네임 getName, editName 해야 됨

--- frontend/src/Utils/User/getName.js
+++ frontend/src/Utils/User/getName.js
@@ -4,7 +4,6 @@ import API from '../../API/api';
 export const getName = async (token, setLoading) => {
   // 이미 로딩 중인 경우 함수 실행을 중지
   if (setLoading) setLoading(true);
-
   const endpoint = `/api/user/nickname/get`;  // bucketListId를 URL에 포함시킴
 
   try {
@@ -12,17 +11,15 @@ export const getName = async (token, setLoading) => {
     const response = await API.patch(endpoint, {
       headers: {
         Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',  // 헤더 설정
+        'Content-Type': 'accept: */*',  // 헤더 설정
       },
     });
 
     // 성공적으로 데이터를 받으면 처리할 로직
     console.log('데이터:', response.data);
     return response.data;  // 필요한 데이터 반환
-
   } catch (error) {
     console.error('API 오류: ', error);
-    alert('닉네임 불러오기에 실패했습니다. 다시 시도해주세요.');
   } finally {
     if (setLoading) setLoading(false);  // 로딩 상태 해제
   }
--- frontend/src/pages/Search/Search.jsx
+++ frontend/src/pages/Search/Search.jsx
@@ -2,20 +2,54 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { IoSearch } from "react-icons/io5";
 import * as S from "./SearchStyle";
-import { users } from "./data";
 import BackButton from "../../components/common/backbutton/backbutton";
 import Error from "../../components/Error/Error";
+import { searchId } from "../../Utils/User/searchId";
 
 export default function Search() {
   const nav = useNavigate();
-  const [load, setLoad] = useState(true);
+  const [load, setLoad] = useState(false);
   const [shouldAnimate, setShouldAnimate] = useState(false);
+  const [users, setUsers] = useState([]);
+  const token = localStorage.getItem("token");
+  const [keyword, setKeyword] = useState("");
+  const [loading, setLoading] = useState(false); // 로딩 상태
+  const [result, setResult] = useState("SUCCESS");
 
   useEffect(() => {
-    setShouldAnimate(true);
-    const timer = setTimeout(() => setShouldAnimate(false), 1000); // 애니메이션 지속 시간 (0.5초)과 동일
+    const loadUsers = async () => {
+      if (!keyword.trim()) {
+        setUsers([]); // 검색어가 없으면 사용자 목록 초기화
+        setLoad(false); // 로딩 상태 초기화
+        return;
+      }
+      setLoading(true);
+      try {
+        const data = await searchId(token, keyword, loading);
+        setUsers(data.data);
+        setResult(data.result);
+        setLoad(true);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadUsers();
+  }, [keyword]);
+
+  const handleSearch = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShouldAnimate(false), 50); // 애니메이션 지속 시간 (0.5초)과 동일
     return () => clearTimeout(timer);
-  }, []);
+  }, [shouldAnimate]);
+
+  useEffect(() => {
+    setShouldAnimate(true); // 새로운 검색어 입력 시 애니메이션 실행
+  }, [keyword]);
 
   return (
     <S.Wrapper>
@@ -27,12 +61,16 @@ export default function Search() {
         <S.Subheader>친구의 목표를 보고 댓글을 남겨보세요.</S.Subheader>
         <S.SearchContainer>
           <IoSearch style={{ color: "black" }} />
-          <S.Searchbar placeholder="이메일/닉네임 입력" />
+          <S.Searchbar
+            placeholder="이메일/닉네임 입력"
+            onChange={handleSearch}
+            value={keyword}
+          />
         </S.SearchContainer>
         {load ? (
-          users.length > 0 ? (
+          result === "SUCCESS" ? (
             <S.List>
-              {users.map((user, index) => (
+              {users.map((user) => (
                 <S.SearchUser
                   key={user.id}
                   shouldAnimate={shouldAnimate}
@@ -47,7 +85,7 @@ export default function Search() {
             <Error text="해당 아이디 유저가 존재하지 않습니다." />
           )
         ) : (
-          <Error text="이메일 또는 닉네임을 입력하시오" />
+          <Error text="이메일 또는 닉네임을 입력하시오 " />
         )}
       </S.Container>
     </S.Wrapper>

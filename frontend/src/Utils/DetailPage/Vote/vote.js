18f96bd1fb934f66f1b89be735c11f1c2e00a077
MJ <nampink2300@gmail.com>
2025-01-12 10:11:48 +0900

    [feat] 투표 완료

--- frontend/src/Utils/DetailPage/Vote/vote.js
+++ frontend/src/Utils/DetailPage/Vote/vote.js
@@ -1,18 +1,16 @@
-import API from '../../API/api';
+import API from '../../../API/api';
 
 export const vote = async (token, bucketListId, isPossible, setLoading, refreshVote) => {
   // 이미 로딩 중인 경우 함수 실행을 중지
   if (setLoading) setLoading(true);
 
-  const endpoint = `/api/votes/${bucketListId}`;  
+  const endpoint = `/api/votes/${bucketListId}?bucketListId=${bucketListId}&isPossible=${isPossible}`;  
   try {
     // API 요청
-    await API.post(endpoint, {
-      bucketListId: bucketListId,
-      isPossible: isPossible
-    }, {
+    await API.post(endpoint, {}, {
       headers: {
         Authorization: `Bearer ${token}`,
+        accept: '*/*',
         'Content-Type': 'application/json',
       },
     });
@@ -22,7 +20,12 @@ export const vote = async (token, bucketListId, isPossible, setLoading, refreshV
 
   } catch (error) {
     console.error('API 오류: ', error);
-    alert('투표에 실패했습니다. 다시 시도해주세요.');
+    // 에러 응답이 있을 경우 메시지 표시
+    if (error.response && error.response.data && error.response.data.message) {
+      alert(error.response.data.message); // 서버에서 반환된 메시지 표시
+    } else {
+      alert('투표에 실패했습니다. 다시 시도해주세요.');
+    }
   } finally {
     if (setLoading) setLoading(false); // 로딩 상태 해제
   }
